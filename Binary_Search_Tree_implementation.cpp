41dc4f815392f8f784930dc4b65505fc287df3e6
Gaurav kushwaha <gaurav121212kush@gmail.com>
2025-11-16 10:06:09 +0530

    Implement deleteNode and inorderSuccessor methods
    Added deletion method for binary search tree, including inorder successor logic.

--- Binary_Search_Tree_implementation.cpp
+++ Binary_Search_Tree_implementation.cpp
@@ -39,12 +39,63 @@ void display (Node* root){  //PreOrder Traversal
         display(root-> left);
         display(root-> right);
 }
-        
+
+// Deletion method
+
+// Gettign inorder successor 
+Node* inorderSuccessor (Node* root){
+        Node* successor = root;
+        while(successor-> left != NULL){
+                successor = successor-> left;
+        }
+        return successor;
+}
+
+Node* deleteNode(Node* root, int key){
+        if(root == NULL) return root;
+        else if (key < root-> data){
+                root-> left = deleteNode(root-> left,key);
+        }
+        else if (key > root-> data){
+                root-> right = deleteNode(root-> right, key);
+        }
+        else{ // case when root-> data == key
+                //casse 1 when 0 child
+                if(root-> left == NULL && root-> right == NULL){
+                        delete root;
+                        return NULL;
+                }
+                //case 2 when right child
+                else if(root->left == NULL){
+                    Node* temp = root->right;
+                    delete root;
+                    return temp;
+                }
+
+                //case 2 when left child
+                else if(root-> right == NULL){
+                        Node* temp = root-> left;
+                        delete root;        //memory free 
+                        return temp;
+                }
+                //case 3 when left and right both child
+                else{
+                        Node* successor = inorderSuccessor(root-> right);
+                        root-> data = successor-> data;
+                        root-> right = deleteNode(root-> right, successor-> data);
+                }
+        }
+        return root;
+}
+
+
 int main(){
-        vector <int> arr = {10,20,50,60,80,100);
+        vector <int> arr = {10,20,50,60,80,100};
         Node* root = buildBST(arr);
         display(root);
-
+        root = deleteNode(root, 50);
+        cout<< " After deletion 50";
+        display(root);
  return 0;
 }
 
